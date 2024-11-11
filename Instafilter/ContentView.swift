8594b93fcb9aa968e786869af4987b879b70da2b
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-11-11 12:00:41 +0500

    day 63 firs and second task

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,17 +5,46 @@
 //  Created by Даниил Сивожелезов on 07.11.2024.
 //
 
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                PhotosPicker(selection: $pickerItems, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
+                    Label("Select photos", systemImage: "photo.artframe")
+                }
+                
+                ShareLink(item: URL(string: "https://www.hackingwithswift.com")!,
+                          subject: Text("Learn Swift Here!"),
+                          message: Text("Check out 100 days of SwiftUI!")) {
+                    Image(systemName: "arrowshape.turn.up.right.fill")
+                }
+            }
+            
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
         }
-        .padding()
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for pickerItem in pickerItems {
+                    if let loadedImage = try await pickerItem.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
     }
 }
 
